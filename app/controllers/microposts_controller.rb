6f6eeafdada90709885cdac76ab999d68a098870
yuma.matsuyama <rinmoku4818@yahoo.co.jp>
2020-07-14 02:49:48 +0000

    post error correction

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,23 +2,16 @@ class MicropostsController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
   
-  def show
-    @user = User.find(params[:id])
-    @microposts = @user.microposts.order(id: :desc).page(params[:page])
-    counts(@user)
-  end
-  
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] ="メッセージを投稿しました"
+      flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
-      
     else
       @microposts = current_user.microposts.order(id: :desc).page(params[:page])
-      flash.now[:danger] = "メッセージの投稿に失敗しました"
-      render "toppages/index"
-    end    
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
+    end
   end
 
   def destroy
@@ -26,27 +19,17 @@ class MicropostsController < ApplicationController
     flash[:success] = 'メッセージを削除しました。'
     redirect_back(fallback_location: root_path)
   end
-  
+
   private
+
   def micropost_params
     params.require(:micropost).permit(:content)
   end
-  
+
   def correct_user
     @micropost = current_user.microposts.find_by(id: params[:id])
     unless @micropost
       redirect_to root_url
     end
   end
-  
-  def require_user_logged_in
-    unless logged_in?
-      redirect_to login_url
-    end
-  end
-
-  def counts(user)
-    @count_microposts = user.microposts.count
-  end
-  
 end
